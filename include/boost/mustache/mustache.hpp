f49673bca2c65d9cf345fec3e138f2eaf191296c
Peter Dimov <pdimov@gmail.com>
2022-12-27 17:50:32 +0200

    Initial commit

---
+++ include/boost/mustache/mustache.hpp
@@ -0,0 +1,18 @@
+#ifndef BOOST_MUSTACHE_MUSTACHE_HPP_INCLUDED
+#define BOOST_MUSTACHE_MUSTACHE_HPP_INCLUDED
+
+// Copyright 2022 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+namespace boost
+{
+namespace mustache
+{
+
+// ...
+
+} // namespace mustache
+} // namespace boost
+
+#endif // #ifndef BOOST_MUSTACHE_MUSTACHE_HPP_INCLUDED
---
+++ test/quick.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/mustache.hpp>
+
+int main()
+{
+}
