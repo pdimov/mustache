ed7db4e96526bfcfbf97d3ee34677d3043d97d85
Peter Dimov <pdimov@gmail.com>
2022-12-29 20:27:03 +0200

    Update test/render_comment.cpp

--- test/render_comment.cpp
+++ test/render_comment.cpp
@@ -3,16 +3,63 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/mustache/render.hpp>
-#include <boost/core/lightweight_test.hpp>
+#include <boost/core/detail/lwt_unattended.hpp>
 #include <string>
+#include <iostream>
+#include <algorithm>
+
+int errors = 0;
+
+static void test( boost::core::string_view tmpl, boost::json::value const& data, boost::json::object const& partials, boost::core::string_view expected )
+{
+    // entire buffer
+
+    {
+        std::string result;
+        boost::mustache::render( tmpl, result, data, partials );
+
+        if( result != expected )
+        {
+            std::cerr << "Test failed: result '" << result << "', expected '" << expected << "'" << std::endl;
+            ++errors;
+        }
+    }
+
+    // chunked
+
+    for( std::size_t i = 1; i <= 8; ++i )
+    {
+        boost::mustache::renderer rd( data, partials );
+
+        boost::core::string_view in = tmpl;
+        std::string result;
+
+        while( !in.empty() )
+        {
+            std::size_t m = std::min( i, in.size() );
+
+            rd.render_some( { in.data(), m }, result );
+            in.remove_prefix( m );
+        }
+
+        rd.finish( result );
+
+        if( result != expected )
+        {
+            std::cerr << "Test (i=" << i << ") failed: result '" << result << "', expected '" << expected << "'" << std::endl;
+            ++errors;
+        }
+    }
+}
 
 int main()
 {
-    std::string st;
+    boost::core::detail::lwt_unattended();
 
-    boost::mustache::render( "prefix * {{! comment }} * suffix", st, {}, {} );
+    test( "prefix{{!comment}}suffix", {}, {}, "prefixsuffix" );
+    test( "prefix * {{! comment\n  comment  \n\n comment }} * suffix", {}, {}, "prefix *  * suffix" );
+    test( "prefix\n    {{! comment }}\nsuffix\n", {}, {}, "prefix\nsuffix\n" );
+    test( "prefix\r\n    {{! comment }}\r\nsuffix\r\n", {}, {}, "prefix\r\nsuffix\r\n" );
 
-    BOOST_TEST_EQ( st, std::string( "prefix *  * suffix" ) );
-
-    return boost::report_errors();
+    return errors;
 }
